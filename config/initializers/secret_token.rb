54bcdf4c456a61634c6740ac340e8aa04c7f54f0
Ivsucram <ivsucram@gmail.com>
2013-12-10 21:10:46 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Costorm::Application.config.secret_key_base = '3efea46d0e1704acde49616293ec14eb0ceb9f675e25e39b602b9c3b1ceeea35f41313d793c9c739cc80e3cef3b3ad07c79f82841bfa2c2824c26a5021e07494'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Costorm::Application.config.session_store :cookie_store, key: '_costorm_session'
